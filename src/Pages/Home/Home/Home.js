51a1e37d3c499db971bd48c5da8802250e7e29b6
bappy-ahashan <bappyahashan@gmail.com>
2023-08-03 16:29:47 +0600

    set up the env and install react-router-dom,tailwind,daisy ui,and create footer and navigation bar and carosel

---
+++ src/Pages/Home/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Bannar from '../Bannar/Bannar';
+
+const Home = () => {
+    return (
+        <div>
+            <Bannar></Bannar>
+            <h1>this is home components</h1>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/Pages/Shared/Navbar/Navbar.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../../../assets/logo.svg';
+
+const Navbar = () => {
+    const navitems = <>
+        <li><Link to='/'>Home</Link></li>
+        <li><Link>About</Link></li>
+        <li><Link>Service</Link></li>
+        <li><Link>Blog</Link></li>
+        <li><Link>Content</Link></li>
+    </>
+    return (
+        <div>
+            <div className="navbar bg-base-100 mb-20">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                            <img src={logo} alt="" />
+                        </label>
+                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                            {navitems}
+                        </ul>
+                    </div>
+                    <Link className="btn btn-ghost normal-case text-xl">
+                        <img src={logo} alt="" />
+                    </Link>
+                </div>
+                <div className="navbar-center hidden lg:flex">
+                    <ul className="menu menu-horizontal px-1">
+                        {navitems}
+                    </ul>
+                </div>
+                <div className="navbar-end">
+                    <Link><button className="btn btn-outline btn-secondary">Appointment</button></Link>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
